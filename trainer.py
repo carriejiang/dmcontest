c8f11325a440042e8ec95392a36fb8af84bdd0a0
carriejiang <carriejiang@Carries-MacBook-Pro.local>
2014-12-10 21:38:32 -0800

    remove superfluous files

--- trainer.py
+++
@@ -1,9 +0,0 @@
-from trainingData import TrainingData
-
-class Trainer(object):
-	def __init__(self):
- 		#super(Trainer, self).__init__()
-		self.data = TrainingData()
-		
-	def train (self, csv_classList, csv_trainedClass):
-		self.data.train(csv_classList, csv_trainedClass)
