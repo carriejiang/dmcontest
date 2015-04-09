cb84c31bd52d92b7abbd16d392ae983db932867c
carriejiang <carriejiang03@gmail.com>
2015-04-08 19:57:15 -0700

    Delete trainer_cat.py

--- trainer_cat.py
+++
@@ -1,9 +0,0 @@
-from trainingData_cat import TrainingData
-
-class CatTrainer(object):
-	def __init__(self):
- 		#super(Trainer, self).__init__()
-		self.data = TrainingData()
-		
-	def train (self, csv_classList, csv_trainedClass):
-		self.data.train(csv_classList, csv_trainedClass)
